c23a09ae3c1d96f05ed9a77198fda8ca1539dd7f
joyroy366 <joy305061@gmail.com>
2024-08-22 09:30:25 +0600

    Add files via upload

---
+++ 263A Beautiful Matrix.py
@@ -0,0 +1,10 @@
+
+mat=[list(map(int,input().split())) for _ in range(5)]
+for i in range(5):
+    for j in range(5):
+        if mat[i][j]==1:
+            r=i+1
+            c=j+1
+print(abs(r-3)+abs(c-3))
+
+
